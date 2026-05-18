30d6549aa72810cd832595ff5729fc799b405e71
Olivér Viktor Halász <viktoroliver.halasz@tesco.com>
2026-05-18 12:28:28 +0200

    style: full-size map

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,17 +10,15 @@ const MAPBOX_ACCESS_TOKEN = (import.meta as any).env.VITE_MAPBOX_ACCESS_TOKEN
 
 function Home() {
   return (
-    <div className="p-8">
-      <Map
-        mapboxAccessToken={MAPBOX_ACCESS_TOKEN}
-        initialViewState={{
-          longitude: 19.076422156938513,
-          latitude: 47.55561160380166,
-          zoom: 14,
-        }}
-        style={{ width: 1000, height: 700 }}
-        mapStyle="mapbox://styles/robertczobor/clnu2vyeo00n801qw3eyz5fm3"
-      />
-    </div>
+    <Map
+      mapboxAccessToken={MAPBOX_ACCESS_TOKEN}
+      initialViewState={{
+        longitude: 19.076422156938513,
+        latitude: 47.55561160380166,
+        zoom: 14,
+      }}
+      style={{ height: '100%', width: '100%', position: 'relative' }}
+      mapStyle="mapbox://styles/robertczobor/clnu2vyeo00n801qw3eyz5fm3"
+    />
   )
 }
